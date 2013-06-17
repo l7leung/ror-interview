fbca8c21a81f6ae55f10c19d9622b72d2ca3b6e9
Louis Leung <to.louis.leung@gmail.com>
2013-06-17 16:59:58 -0400

    project for ror test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carta::Application.config.secret_token = 'ecc215b12d25d77d86d9df186cdef551baef5fa2419c2338a1fb85004b21a2f05b7214d2ac12d9ed6eb9a7d74f9534c20df21f75aff590930b51ce3eb5b71eff'
